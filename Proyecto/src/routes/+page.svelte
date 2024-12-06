b4925c4bba7bc3b817d1495d806c76571d61e704
fperuch <98467208+fperuch@users.noreply.github.com>
2024-12-06 13:49:16 -0600

    Creacion de pagina

--- Proyecto/src/routes/+page.svelte
+++ Proyecto/src/routes/+page.svelte
@@ -10,12 +10,12 @@
     }
 </script>
 
-<div class="p-5 flex flex-row h-full max-h-full">
-    <div class="h-full w-1/2 mx-2 rounded-md border-black border overflow-y-scroll">
+<div class="p-5 flex flex-row h-full max-h-full bg-black">
+    <div class="h-full w-1/2 mx-2 rounded-md font-extrabold border-white bg-amber-200 border overflow-y-scroll">
         <CodeMirror bind:value={texto} on:change={analizar}/>
     </div>
-    <div class="rounded-md border-black border-2 h-full w-1/2 mx-2">
-        <p class="text-red-500">
+    <div class="rounded-md border-white bg-amber-200 border-2 h-full w-1/2 mx-2">
+        <p class="font-extrabold">
             {resultado}
         </p>
     </div>
